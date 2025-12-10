c722a6374054f649a79c1a8f736f2f027017332a
Yuliia Panchenko <72158175+J-Panchenko@users.noreply.github.com>
2025-12-10 11:27:03 +0200

    add logo to marketing header and remove unused SVG files

--- components/headers/marketing-header/marketing-header.tsx
+++ components/headers/marketing-header/marketing-header.tsx
@@ -3,6 +3,8 @@
 import dynamic from "next/dynamic";
 import Link from 'next/link';
 import NavLink from './nav-link';
+import Image from 'next/image';
+import logo from '@/public/logo.svg';
 
 const ThemeChangerComponent = dynamic(() => import("@/components/theme-changer").then(mod => mod.default), {
   ssr: false,
@@ -10,9 +12,17 @@ const ThemeChangerComponent = dynamic(() => import("@/components/theme-changer")
 
 export function MarketingHeader() {
   return (
-    <header className="w-full px-4 py-2 border-b flex items-center justify-between bg-accent dark:bg-accent-dark">
-      <Link href="/" className="text-xl font-bold">
-        Workout AI
+    <header className="w-full px-4 py-2 border-b border-n200 dark:border-n200-dark flex items-center justify-between bg-n100 dark:bg-n100-dark">
+      <Link href="/" className="text-lg font-bold flex items-center justify-center space-x-2">
+        <Image
+          src={logo}
+          alt="Workout AI Logo"
+          width={40}
+          height={40}
+        />
+        <h1 className="text-primary dark:text-primary-dark">
+          Workout AI
+        </h1>
       </Link>
       <nav>
         <ul>
